3f3343b6f81ee9900828db0aa90405b0f06b6371
Winnie Mendoza <wrm956@gmail.com>
2022-07-07 09:32:53 -0400

    solution

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,12 @@
 class Dev < ActiveRecord::Base
+  has_many :freebies
+  has_many :companies, through: :freebies
 
+  def received_one?(item_name)
+    freebies.where(item_name: item_name).any?
+  end
+
+  def give_away(dev, freebie)
+    freebie.update(dev: dev)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,18 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
+google = Company.create(name: "Google", founding_year: 1998)
 Company.create(name: "Facebook", founding_year: 2004)
 Company.create(name: "Dunder Mifflin", founding_year: 2002)
 Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
 Dev.create(name: "Mr. Meseeks")
 Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
+Freebie.create(item_name: "Mona Stuffed Animal", value: 2, dev: rick, company: google)
+Freebie.create(item_name: "Mona Stuffed Animal", value: 2, dev: morty, company: google)
 
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
